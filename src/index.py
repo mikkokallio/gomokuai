3162444c2eeb5c3b5c03825676ab087e5cdf70cb
Mikko Kallio <mikko.m.kallio@gmail.com>
2022-11-26 23:01:23 +0200

    Add default

--- src/index.py
+++ src/index.py
@@ -9,7 +9,7 @@ def main():
     parser = argparse.ArgumentParser(description='Runs gomoku for two AI players',
                                      epilog=f'Player names must be from the following list:\n{list(AI_PLAYERS.keys())}')
     parser.add_argument('-a', '--alternate', action='store_true', help='plays the game twice with players switching colors')
-    parser.add_argument('-r', '--repeat', type=int, metavar='n', help='repeats the game n times')
+    parser.add_argument('-r', '--repeat', type=int, metavar='n', default=1, help='repeats the game n times')
     parser.add_argument('-c', '--csv', action='store_true', help='hides normal output and instead prints only game result as a comma-separated string')
     parser.add_argument('-s', '--store', action='store_true', help='saves information about played games')
     parser.add_argument('player1', choices=AI_PLAYERS)
